11ee454153d6dcc243999a0514479614a0be279b
wangjingyi <wangjingyi@tfzq.com>
2023-10-20 15:24:57 +0800

    电子书表的建立和查询的实现

---
+++ src/main/java/com/wjy/springtest/mapper/EbookMapper.java
@@ -0,0 +1,30 @@
+package com.wjy.springtest.mapper;
+
+import com.wjy.springtest.domain.Ebook;
+import com.wjy.springtest.domain.EbookExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EbookMapper {
+    long countByExample(EbookExample example);
+
+    int deleteByExample(EbookExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Ebook record);
+
+    int insertSelective(Ebook record);
+
+    List<Ebook> selectByExample(EbookExample example);
+
+    Ebook selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByExample(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByPrimaryKeySelective(Ebook record);
+
+    int updateByPrimaryKey(Ebook record);
+}
